677d44a7892fcc2f5cc6c8e72e98b3b1fe39017e
ShopifyDadSupport <webdeveloper@unimedint.com>
2023-10-18 17:34:59 -0700

    update code server file and build folder

--- sealAppFronted/src/components/ShopifyPage/ShopifyCreatePage.js
+++ sealAppFronted/src/components/ShopifyPage/ShopifyCreatePage.js
@@ -2,25 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './shopifyCreatePage.css';
 
 const ShopifyCreatePage = () => {
-  // const [htmlDocument, setHtmlDocument] = useState(null);
-
-  const url = "https://admin.shopify.com/store/priorcric/reports/sessions_over_time?since=-7d&until=today&over=day";
-
-fetch(url)
-  .then((response) => {
-    if (response.ok) {
-      return response.text();
-    } else {
-      throw new Error(`Failed to fetch URL. Status code: ${response.status}`);
-    }
-  })
-  .then((htmlContent) => {
-    // Now, htmlContent contains the HTML document from the URL.
-    console.log(htmlContent);
-  })
-  .catch((error) => {
-    console.error(`Request error: ${error.message}`);
-  });
+  // const [htmlDocument, setHtmlDocument] = useState(null)
 
   return (
     <div>
