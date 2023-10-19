05f6f3d2037b06a0d341f456e2717a7d592dac15
ShopifyDadSupport <webdeveloper@unimedint.com>
2023-10-19 13:30:25 -0700

    add gdpr webhooks server file

--- gdpr/cust_data_request.js
+++ gdpr/cust_data_request.js
@@ -1,10 +1,11 @@
 const crypto = require("crypto");
 function verifyWebhook(data, hmacHeader, SHOPIFY_APP_SECRET) {
-    console.log("data, hmacHeader,SHOPIFY_APP_SECRET:-",data, hmacHeader, SHOPIFY_APP_SECRET);
+    // console.log("data, hmacHeader,SHOPIFY_APP_SECRET:-",data, hmacHeader, SHOPIFY_APP_SECRET);
     const calculatedHmac = crypto
       .createHmac('sha256', SHOPIFY_APP_SECRET)
       .update(data)
       .digest('base64');
+      console.log("value boolean ::",crypto.timingSafeEqual(Buffer.from(hmacHeader), Buffer.from(calculatedHmac)),"===================",calculatedHmac)
     return crypto.timingSafeEqual(Buffer.from(hmacHeader), Buffer.from(calculatedHmac));
   }
 
