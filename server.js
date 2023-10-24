d68ad70c443abf9cac020b256546c510fd9bfcce
ShopifyDadSupport <webdeveloper@unimedint.com>
2023-10-24 16:03:28 -0700

    update fronted code

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.get("/shopify/callback", async (req, res) => {
             //     'Location': redirectURL
             // });
             // res.end();
-            // const redirect_uri = `https://admin.shopify.com/store/${shop__name}/apps/${accessTokenPayload.client_id}`;
+            const redirect_uri = `https://admin.shopify.com/store/${shop__name}/apps/${accessTokenPayload.client_id}`;
 
             // console.log("djkasssssssssssssssssssssssssssssssssssssssss=:=",accessTokenPayload,shop__name,"djksdhjad::-",redirect_uri,req.query.shop);
 
